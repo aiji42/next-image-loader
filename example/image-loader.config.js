b1c271d451282225ab4e0aefaa364ca994b97a69
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-28 22:27:40 +0900

    chore: update example

--- example/image-loader.config.js
+++ example/image-loader.config.js
@@ -1,6 +1,7 @@
-import { imageLoader } from 'next-image-loader/build/image'
+import { imageLoader } from 'next-image-loader/build/image-loader'
 
-imageLoader.set(({ src, width, quality }) =>
-  `${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}/demo/image/upload/w_${width}/q_${
+imageLoader.loader = ({ src, width, quality }) => {
+  return `${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}/demo/image/upload/w_${width}/q_${
     quality || 75
-  }/${src.replace(/^\//, '')}`)
+  }/${src.replace(/^\//, '')}`
+}
