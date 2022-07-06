a221772bc3b9aa1070cf4786e566c979a4223e8a
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-07-06 11:09:21 +0900

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -2,6 +2,7 @@ const withImageLoader = require('next-image-loader')
 
 module.exports = withImageLoader({
   reactStrictMode: true,
+  experimental: { images: { allowFutureImage: true } },
   env: {
     NEXT_PUBLIC_IMAGE_DOMAIN: 'https://res.cloudinary.com'
   }
--- example/pages/future.tsx
+++ example/pages/future.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from 'next/future/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
