9c32ad43c1d6dc716d3fa33905f5ca12c0869125
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-11-22 08:51:28 +0900

    chore: update example nextjs v13

--- example/next.config.js
+++ example/next.config.js
@@ -2,7 +2,7 @@ const { withImageLoader } = require('next-image-loader')
 
 module.exports = withImageLoader({
   reactStrictMode: true,
-  experimental: { images: { allowFutureImage: true } },
+  swcMinify: true,
   env: {
     NEXT_PUBLIC_IMAGE_DOMAIN: 'https://res.cloudinary.com'
   }
